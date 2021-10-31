51274ae5c8a2bea9ab31b62b75ba272b0bc879cc
ashok9git <30350155+ashok9git@users.noreply.github.com>
2021-10-31 20:09:07 +0530

    update

--- dev/main.tf
+++ dev/main.tf
@@ -4,3 +4,9 @@ resource "google_storage_bucket" "storage-1" {
   force_destroy = true
 
 }
+resource "google_storage_bucket" "storage-1" {
+  name          = "storage-dev-malli-ext"
+  location      = "EU"
+  force_destroy = true
+
+}
